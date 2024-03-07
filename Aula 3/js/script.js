4ae904619aec553cb6749b352fb8ee1d489a33fb
Iago Menezes <124791185+IagoMenezes19@users.noreply.github.com>
2024-03-06 23:10:10 -0300

    Add files via upload

--- Aula 3/js/script.js
+++ Aula 3/js/script.js
@@ -1,19 +1,65 @@
-var resultado = document.getElementById('resultado')
+var result = document.getElementById('resultado')
 
 function peganumero1(){
-    var prinumber = document.getElementById ('prinumber').value
-    console.log('Primeiro numero: ', prinumber)
+
+    var n1 = Number(document.getElementById('n1').value) 
+    console.log('n1:  ', n1)
+
 }
 
 function peganumero2(){
-    var segnumber = document.getElementById ('prinumber').value
-    console.log('Primeiro numero: ', segnumber)
+
+    var n2 = Number(document.getElementById('n2').value)
+    console.log('n2:  ', n2)
+   
+
 }
 
 function soma(){
-    var prinumber = parseInt(document.getElementById('prinumber').value)
-    var segnumber = parseInt(document.getElementById('segnumber').value)
-    var soma = prinumber + segnumber
-    console.log('soma: ', soma)
-    resultado.innerHTML = soma
+    var n1 = Number(document.getElementById('n1').value)
+    var n2 = Number(document.getElementById('n2').value)
+    var soma = n1 + n2
+    result.innerHTML = soma
+    console.log ('Soma: ' , soma)
+
+}
+
+function sub(){
+
+    var n1 = Number(document.getElementById('n1').value) 
+    var n2 = Number(document.getElementById('n2').value)
+    var sub = n1 - n2
+    result.innerHTML = sub
+    console.log ('sub: ' , sub)
+
+
+}
+
+function mult(){
+
+    var n1 = Number(document.getElementById('n1').value) 
+    var n2 = Number(document.getElementById('n2').value)
+    var mult = n1 * n2
+    result.innerHTML = mult
+    console.log ('mult: ' , mult)
+
+
+}
+
+function divi(){
+
+    var n1 = Number(document.getElementById('n1').value) 
+    var n2 = Number(document.getElementById('n2').value)
+    var divi = n1 / n2
+    result.innerHTML = divi
+    console.log ('divi: ' , divi)    
+
+
+}
+
+function limp(){
+    result.innerHTML = " "
+    numero1.innerHTML = ' '
+    numero2.innerHTML = ' '
+
 }
--- exerciciosLampada/js/script.js
+++ exerciciosLampada/js/script.js
@@ -1,36 +1,56 @@
 const ligar = document.getElementById('ligar')
 const desligar = document.getElementById('desligar')
-const lampada = document.getElementById('lamp')
-const restaurar = document.getElementById('restaurar')
+const lampada = document.getElementById('lampada')
+const bduplo = document.getElementById('bduplo')
+const texto = document.getElementById('texto')
+
 
 function estaQuebrada(){
     return lampada.src.indexOf('quebrada') > -1
-    //isso retorna true ou false
+    texto.innerHTML ="LIGADA"
 }
 
-function restaura(){
-    lampada.src = "img/desligada.jpg"
-} 
-
-function lampLigada(){
-    if (!estaQuebrada())
+function lampadaligar(){
+    if(!estaQuebrada()){
     lampada.src = "img/ligada.jpg"
+    }
+    texto.innerHTML ="LIGADA"
 }
 
-function lampDesligada(){
-    if (!estaQuebrada())
+function lampadadesligada(){
+    if(!estaQuebrada()){
     lampada.src = "img/desligada.jpg"
+    }
+    texto.innerHTML ="DESLIGADA"
 }
 
-function lampQuebrada(){
-    lampada.src = "img/quebrada.jpg"
+function lampadaquebrada(){
+    lampada.src= "img/quebrada.jpg"
+    texto.innerHTML ="QUEBRADA"
 }
 
-ligar.addEventListener('click',lampLigada)
-desligar.addEventListener('click',lampDesligada)
+function ligadesliga() {
+    if (bduplo.textContent=='LIGAR'){
+        lampadaligar();
+        bduplo.textContent='DESLIGAR';
+    } 
+    
+    else{
+        lampadadesligada();
+        bduplo.textContent='LIGAR';
+    }
+    texto.innerHTML ="LIGA/DESLIGA"
+ 
+}
 
-lampada.addEventListener('mouseover',lampLigada)
-lampada.addEventListener('mouseleave',lampDesligada)
 
-lampada.addEventListener('dblclick',lampQuebrada)
-restaurar.addEventListener('dbclick',restaura)
+
+ligar.addEventListener('click',lampadaligar)
+desligar.addEventListener('click',lampadadesligada)
+
+lampada.addEventListener('mouseover',lampadaligar)
+lampada.addEventListener('mouseleave',lampadadesligada)
+
+lampada.addEventListener('dblclick',lampadaquebrada)
+
+bduplo.addEventListener('click',ligadesliga)
